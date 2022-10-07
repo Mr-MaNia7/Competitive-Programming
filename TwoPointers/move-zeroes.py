e8b0d077efbcce12b691ebefc500ee55f7e325cb
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-10-07 14:32:11 +0300

    Improve: 364ms

--- TwoPointers/move-zeroes.py
+++ TwoPointers/move-zeroes.py
@@ -1,12 +1,21 @@
 from typing import List
-
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                nums.remove(nums[i])
-                nums.append(0)
+        n = len(nums)
+        idx1 = 0
+        idx2 = 1
+        while idx2 < n:
+            if nums[idx1] == 0 and nums[idx2] != 0:
+                nums[idx1], nums[idx2] = nums[idx2], nums[idx1]
+                idx1 += 1
+                idx2 += 1
+            elif nums[idx1] == 0 and nums[idx2] == 0:
+                idx2 += 1
+            else:
+                idx1 += 1
+                idx2 += 1
+            # print(idx1, idx2, nums)
 
 if __name__ == "__main__":
     soln = Solution()
-    soln.moveZeroes([0,0,1])
+    soln.moveZeroes([0,1,0,3,12])
