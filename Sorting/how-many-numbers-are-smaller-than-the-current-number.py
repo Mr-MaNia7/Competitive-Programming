ad60bc1b3c92e9932687dccbc50633c98d290071
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-10-08 09:32:52 +0300

    Improved: 129ms

--- Sorting/how-many-numbers-are-smaller-than-the-current-number.py
+++ Sorting/how-many-numbers-are-smaller-than-the-current-number.py
@@ -2,11 +2,12 @@ from typing import List
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        arr = []
-        for i in range(n):
-          count = 0
-          for j in range(n):
-            if i != j and nums[j] < nums[i]: count += 1
-          arr.append(count)
-        return arr
+        dic = {}
+        for idx, num in enumerate(sorted(nums)):
+            if num not in dic:
+                dic[num] = idx
+        return [dic[num] for num in nums]
+
+if __name__ == "__main__":
+    s = Solution()
+    s.smallerNumbersThanCurrent([8,1,2,2,3])
