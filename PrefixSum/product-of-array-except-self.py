2c610113565e73f95073f18f25549306d17bbc76
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-10-13 09:03:27 +0300

    Improved: 250ms

--- PrefixSum/product-of-array-except-self.py
+++ PrefixSum/product-of-array-except-self.py
@@ -1,24 +1,15 @@
 from typing import List
 
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix = 1
-        suffix = 1
-        prelist = []
-        suflist = []
-        ans = []
-        for num in nums:
-            prefix *= num
-            prelist.append(prefix)
-        for num in nums[::-1]:
-            suffix *= num
-            suflist.append(suffix)
-        n = len(nums)
-        for i in range(n):
-            p = prelist[i-1] if i-1 >= 0 else 1
-            s = suflist[n-1-(i+1)] if n-1-(i+1) >= 0 else 1
-            ans.append(s * p)
-        return ans 
+    def productExceptSelf(self, nums: List[int]) -> List[int]:        
+        prefix, suffix, n = 1, 1, len(nums)
+        ans = [1] * n
+        for idx in range(n):
+            ans[idx] *= prefix
+            ans[-idx-1] *= suffix
+            prefix *= nums[idx]
+            suffix *= nums[-idx-1]
+        return ans
         
 if __name__ == "__main__":
     s = Solution()
