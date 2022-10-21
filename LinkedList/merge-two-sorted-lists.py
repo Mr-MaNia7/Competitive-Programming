2c1785215532677d6bf3bcbe48213f299ef73d08
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-10-21 10:34:50 +0300

    Solved: 3att 47min

---
+++ LinkedList/merge-two-sorted-lists.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        curr = dummy = ListNode()
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1, curr = list1.next, list1
+            else:   
+                curr.next = list2
+                list2, curr = list2.next, list2
+        if list1 or list2:
+            curr.next = list1 if list1 else list2
+        return dummy.next
+
+if __name__ == "__main__":
+    def printListNode(lst):
+        while lst:
+            print(lst.val, end=" ")
+            lst = lst.next
+    soln = Solution()
+    printListNode(soln.mergeTwoLists(
+        list1=ListNode(1, ListNode(2, ListNode(4))),
+        list2=ListNode(2, ListNode(3, ListNode(4)))
+        ))
