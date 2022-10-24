ff9b841d4f7e6355df9cd17b1670df7391e051eb
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-10-24 12:12:09 +0300

    Improved: 1att 30ms

--- Heap/last-stone-weight.py
+++ Heap/last-stone-weight.py
@@ -3,20 +3,12 @@ from typing import List
 
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        if len(stones) == 1:
-            return stones[0]
         maxheap = [-stone for stone in stones]
         heapq.heapify(maxheap)
-        while len(maxheap) > 1:
-            heav = heapq.nsmallest(2, maxheap)
-            x = -heav[1]
-            y = -heav[0]
-            if x == y:
-                heapq.heappop(maxheap)
-                heapq.heappop(maxheap)
-            else:
-                heapq.heappop(maxheap)
-                heapq.heapreplace(maxheap, -(y-x))
+        while len(maxheap) > 1 and maxheap[0] != 0:
+            y = -heapq.heappop(maxheap)
+            x = -heapq.heappop(maxheap)
+            heapq.heappush(maxheap, -(y - x))
         return -maxheap[0] if maxheap else 0
 
 if __name__ == "__main__":
