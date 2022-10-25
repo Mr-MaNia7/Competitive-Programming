c9c252a31493523b5e1176e623cc13fd63e765f7
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-10-25 19:36:15 +0300

    Improved: 1att 527ms

--- Sorting/find-the-kth-largest-integer-in-the-array.py
+++ Sorting/find-the-kth-largest-integer-in-the-array.py
@@ -1,11 +1,12 @@
+import heapq
 from typing import List
 
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
         nums = [int(num) for num in nums]
-        nums.sort(reverse=True)
-        return str(nums[k-1])
+        heapq.heapify(nums)
+        return str(heapq.nlargest(k, nums)[k-1])
 
-if __name__ == "__main__":
+if __name__=="__main__":
     s = Solution()
     print(s.kthLargestNumber(nums = ["3","6","7","10"], k = 4))
