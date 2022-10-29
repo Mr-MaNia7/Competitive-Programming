8f1d4d522192de25346f5d6f6e1de83756c9a890
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-10-29 17:29:37 +0300

    Solved: 1att 77min

---
+++ Stack/reverse-substrings-between-each-pair-of-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+        i = 0
+        while i < len(s):
+            c = s[i]
+            if c == '(':
+                stack.append(i)
+            elif c == ')':
+                p = stack.pop()
+                s = s[:p] + s[i-1:p:-1] + s[i+1:]
+                i -= 2
+            i += 1
+        return s
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.reverseParentheses(s = "(ed(et(oc))el)"))
