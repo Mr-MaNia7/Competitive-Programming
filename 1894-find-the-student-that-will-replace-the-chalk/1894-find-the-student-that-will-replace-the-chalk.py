a14ae2b7ce2c8ff0281091ea0df5654278cd399e
Abdulkarim Getachew <abdulkarimgmohammed@gmail.com>
2022-11-24 11:11:27 +0300

    Time: 1363 ms (70.86%), Space: 28.1 MB (72.35%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
@@ -1,10 +1,7 @@
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
         r = k % sum(chalk)
-        prefixsum = [0] * (len(chalk) + 1)
-        for i in range(len(chalk)):
-            prefixsum[i+1] = prefixsum[i] + chalk[i]
-            if prefixsum[i+1] > r: return i
-        # for i, p in enumerate(prefixsum[1:]):
-        #     if p > r:
-        #         return i
+        for i, c in enumerate(chalk):
+            if c > r:
+                return i
+            r -= c
