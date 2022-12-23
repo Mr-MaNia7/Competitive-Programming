ad96255e3d38185af996e00b3dc55743e18c937b
Abdulkarim Getachew <abdulkarimgmohammed@gmail.com>
2022-12-23 07:21:44 +0300

    Time: 61 ms (20.50%), Space: 13.8 MB (95.69%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -1,9 +1,3 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        f = ""
-        for i, c in enumerate(address):
-            if c == ".":
-                f += "[.]"
-            else:
-                f += c
-        return f
+        return re.sub("[.]", "[.]", address)
