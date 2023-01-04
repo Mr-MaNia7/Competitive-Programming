f53cc82bb4d63cef9a4187acb70c278e3bdba294
Abdulkarim Getachew <abdulkarimgmohammed@gmail.com>
2023-01-04 23:24:01 +0300

    Time: 417 ms (66.67%), Space: 28.9 MB (20.83%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
@@ -1,13 +1,5 @@
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        n = len(arr)
-        prefix = [1] * (n+1)
-        ans = []
-        for i in range(1, n+1):
-            prefix[i] = prefix[i-1] ^ arr[i-1]
-        print(prefix)
-        for q in queries:
-            s = q[0]
-            e = q[1]
-            ans.append(prefix[e+1] ^ prefix[s])
-        return ans
+        for i in range(len(arr)-1):
+            arr[i+1] ^= arr[i]
+        return [arr[e] ^ arr[s-1] if s else arr[e] for s, e in queries]
