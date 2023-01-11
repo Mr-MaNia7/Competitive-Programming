7eaf62d419bbdf4fab0645a6fb7d75ce33f63f0a
Abdulkarim Getachew <abdulkarimgmohammed@gmail.com>
2023-01-11 16:48:48 +0300

    Time: 36 ms (84.07%), Space: 13.8 MB (59.63%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        memory = set()
+        while n != 1:
+            n = sum([int(i)**2 for i in str(n)])
+            if n in memory:
+                return False
+            else:
+                memory.add(n)
+        return True
