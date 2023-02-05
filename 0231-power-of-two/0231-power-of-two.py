2503b7b6b5b81a8dd7c98116b414674b8a8fcbc5
Abdulkarim Getachew <abdulkarimgmohammed@gmail.com>
2023-02-05 23:52:38 +0300

    Time: 38 ms (50.96%), Space: 13.7 MB (93.81%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,9 +1,6 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        for i in range(n):
+        for i in range(32):
             if n == 2**i:
                 return True
-            elif 2**i > n:
-                return False
-        else:
-            return False
+        return False
